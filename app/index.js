b7f0007202464f9fcfb1739086c7a1229ea0f749
Gopichandu923 <gopichandu6080@gmail.com>
2025-06-01 13:25:56 +0530

    removed default folders

---
+++ app/index.js
@@ -0,0 +1,12 @@
+import { Button, Text, View } from "react-native";
+import { useNavigation, Redirect } from "expo-router";
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
